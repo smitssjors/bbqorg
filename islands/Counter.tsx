f4786f5486ff3181da6a8bd6c2f86d1690f623f6
Sjors Smits <smits.sjors@gmail.com>
2023-06-23 20:46:46 +0200

    fix css

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -8,7 +8,7 @@ interface CounterProps {
 export default function Counter(props: CounterProps) {
   return (
     <div class="flex gap-2 w-full">
-      <p class="flex-grow-1 font-bold text-xl">{props.count}</p>
+      <p class="grow font-bold text-xl">{props.count}</p>
       <Button onClick={() => props.count.value -= 1}>-1</Button>
       <Button onClick={() => props.count.value += 1}>+1</Button>
     </div>
