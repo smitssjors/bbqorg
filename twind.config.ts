f7ead80c3f918269747473d7f892edd5475079bc
Sjors Smits <smits.sjors@gmail.com>
2023-06-23 20:44:54 +0200

    upgrade twind

--- twind.config.ts
+++ twind.config.ts
@@ -1,5 +1,9 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { Options } from "$fresh/plugins/twindv1.ts";
+import { defineConfig } from "https://esm.sh/@twind/core@1.1.3";
+import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 
 export default {
+  ...defineConfig({ presets: [presetAutoprefix(), presetTailwind()] }),
   selfURL: import.meta.url,
-} as Options;
+} as unknown as Options;
